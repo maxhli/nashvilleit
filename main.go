5924ad99fd8e48c9483e8e707de39a959555143c
Max Li <max.li@live.com>
2017-12-06 19:56:41 -0600

    initial one.

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+
+import (
+	"log"
+	"net/http"
+	"fmt"
+)
+
+func myhandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(w, "Hellofdsfsdafd!")
+}
+func main() {
+	http.Handle("/public/", http.StripPrefix("/public/",
+		http.FileServer(http.Dir("public"))))
+
+	http.HandleFunc("/hello", myhandler)
+
+	// http.Handle("dfdf", fs)
+
+	log.Println("Listening...")
+	http.ListenAndServe(":3000", nil)
+}
