394369111229a23105e38c921d66ec1d6fdb8b0f
Max Li <max.li@live.com>
2017-12-09 12:18:10 -0600

    simplify files.

--- main.go
+++ main.go
@@ -6,18 +6,37 @@ import (
 	"net/http"
 	"fmt"
 	"os"
+	"html/template"
+	"io/ioutil"
 )
 
 func myhandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "Hellofdsfsdafd!")
+	fmt.Fprintln(w, "hehhehehehheheheheheh!")
 }
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := "templates/index.tmpl.html"
+	t, _ := template.ParseFiles(
+		title, "templates/header.tmpl.html", "templates/nav.tmpl.html")
+	fmt.Println("t is: ", t)
+	t.ExecuteTemplate(w, title, nil)
+	}
+
+func RootHandler(res http.ResponseWriter, req *http.Request) {
+	file, _ := ioutil.ReadFile("templates/index.tmpl.html")
+	res.Write(file)
+}
+
 func main() {
+
+	//http.HandleFunc("/", viewHandler)
+	http.HandleFunc("/", RootHandler)
+
 	http.Handle("/public/", http.StripPrefix("/public/",
 		http.FileServer(http.Dir("public"))))
 
 	http.HandleFunc("/hello", myhandler)
 
-	http.HandleFunc("/", myhandler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
